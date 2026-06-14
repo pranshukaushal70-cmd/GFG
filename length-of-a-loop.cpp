13e1cbf2a706be0f7933e4b5377873a4f6584a86
Pranshu Kaushal <pranshukaushal70@gmail.com>
2026-06-14 20:12:31 +0530

    Create length-of-a-loop.cpp

---
+++ length-of-a-loop.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLoop(Node *head) {
+        Node* slow = head;
+        Node* fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                int count = 1;
+                Node* temp = slow->next;
+                while (temp != slow) {
+                    count++;
+                    temp = temp->next;
+                }
+                return count;
+            }
+        }
+        return 0; 
+    }
+};
