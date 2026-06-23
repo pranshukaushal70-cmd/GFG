842f907e1f48530ef6dc74cc92cd93d989c90d18
Pranshu Kaushal <pranshukaushal70@gmail.com>
2026-06-24 04:12:36 +0530

    Create bfs-of-graph.cpp

---
+++ bfs-of-graph.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // code here
+        int n=adj.size();
+        int vis[n]={0};
+        vis[0]=1;
+        queue<int> q;
+        q.push(0);
+        vector<int> bfs;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            bfs.push_back(node);
+            for(int it : adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
