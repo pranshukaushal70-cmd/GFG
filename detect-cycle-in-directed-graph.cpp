3e8c7a797741e63c4b61ce0cd70a7fd1aac6c38a
Pranshu Kaushal <pranshukaushal70@gmail.com>
2026-07-03 18:28:04 +0530

    Create detect-cycle-in-directed-graph.cpp

---
+++ detect-cycle-in-directed-graph.cpp
@@ -0,0 +1,36 @@
+class Solution {
+	public:
+	bool isCyclic(int V, vector<vector<int>> &edges) {
+		// code here
+		vector<vector<int>> adj(V);
+		for (auto &it : edges) {
+			int u = it[0];
+			int v = it[1];
+			adj[u].push_back(v);
+		}
+		vector<int> indegree(V, 0);
+		for (int i = 0; i < V; i++) {
+			for (auto it : adj[i]) {
+				indegree[it]++;
+			}
+		}
+		queue<int> q;
+		for (int i = 0; i < V; i++) {
+			if (indegree[i] == 0)
+				q.push(i);
+		}
+		vector<int> ans;
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			ans.push_back(node);
+			for (auto it : adj[node]) {
+				indegree[it]--;
+				if (indegree[it] == 0)
+					q.push(it);
+			}
+		}
+		if(ans.size()==V) return false;
+		return true;;
+	}
+};
